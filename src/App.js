a93cff12057572551bfcd356648855b3b63bd8c9
JayNagpal02 <jaynagpal02@gmail.com>
2024-01-16 16:22:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import GoalList from "./components/GoalList/GoalList";
+import NewGoal from "./components/NewGoal/NewGoal";
 
 function App() {
+  /* 
+    we have a create a const named courseGoals which is a array of objects 
+    which contains two things an 'id' and a 'text' of objects.
+   */
+  const courseGoals = [
+    { id: "cg1", text: "Finish the Course" },
+    { id: "cg2", text: "Learn all about the Course Main Topic" },
+    { id: "cg3", text: "Help other students in the Course Q&A" },
+  ];
+
+  const addNewGoalHandler = (newGoal) => {
+    courseGoals.push(newGoal);
+    console.log(courseGoals);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="course-goals">
+      <h2>Course Goals</h2>
+      <NewGoal onAddGoal={addNewGoalHandler} />
+      <GoalList goals={courseGoals} />
     </div>
   );
 }
